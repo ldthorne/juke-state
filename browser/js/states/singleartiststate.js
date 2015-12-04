23142d58fe1991a78a4a1580fc22d68a7bd98a35
opperator <julie.e.oppermann@gmail.com>
2015-12-04 17:36:09 -0500

    completed everything except the bonus

--- browser/js/states/singleartiststate.js
+++ browser/js/states/singleartiststate.js
@@ -1,8 +1,24 @@
 app.config(function ($stateProvider) {
-    $stateProvider.state('singleArtist', {
+    
+    $stateProvider
+    .state('singleArtist', {
         url: '/artists/:id',
         templateUrl: "/templates/singleartisttemplate.html",
-       	controller: 'ArtistCtrl'
+        controller: 'ArtistCtrl'
+    })
+    .state('singleArtist.albums', {
+    	url: '/albums',
+    	templateUrl: "/templates/singleartistalbumstemplate.html"
+    	
+    })
+    .state('singleArtist.songs', {
+    	url: '/songs',
+    	templateUrl: "/templates/singleartistsongstemplate.html"
+    	
+    	
+    })    
+       
+    
  		
-    });
+   
 });
